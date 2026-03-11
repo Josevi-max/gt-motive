2af880e85ff5b11752531cd3fb69d838989b45f8
Jose vicente <joseevicentefernandez@gmail.com>
2026-03-11 18:01:27 +0100

    feat: add virtual scroll

--- src/app/features/home/presentation/home/home.ts
+++ src/app/features/home/presentation/home/home.ts
@@ -1,10 +1,10 @@
 import { Component, computed, inject, Signal } from '@angular/core';
 import { VehicleBrand } from '../../domain/models/home.models';
 import { HomeStore } from '../../state/store/home.store';
-
+import { ScrollingModule } from '@angular/cdk/scrolling';
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [ScrollingModule],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
